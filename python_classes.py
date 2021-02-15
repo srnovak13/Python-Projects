cd3c577a1ffd382130c0d25215dca314b20a0eec
srnovak13 <srnovak@gmail.com>
2021-02-15 14:36:56 -0800

    Add python_classes

---
+++ python_classes.py
@@ -0,0 +1,21 @@
+
+# creating class 'Library' and giving  its default values
+class Library:
+    name = ""
+    branch_number = 0
+    address = ""
+    phone_number = 0
+
+class Branch(Library):
+     branch_dir_name = ""
+     employee_count = 0
+
+class Employee(Library):
+    employee_id = 0
+    years_of_employment = 0
+
+    
+    
+    
+     
+    
