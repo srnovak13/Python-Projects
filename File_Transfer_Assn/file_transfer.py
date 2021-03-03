a32b778f493d87154246b3e81b6f12725f0ab21b
srnovak13 <srnovak@gmail.com>
2021-03-03 15:12:55 -0800

    Import file_transfer project

--- File_Transfer_Assn/file_transfer.py
+++ File_Transfer_Assn/file_transfer.py
@@ -1,17 +1,36 @@
-import shutil
+
+#Importing necessary modules
+import shutil 
 import os
-
-# set where the source of the files are
-source = 'C:\\Users\\srnov\\Git Depositories\\Python Projects\\Python-Projects\\File_Transfer_Assn\\Folder_New\\'
-
-#set the destination path to folder B
-destination = 'C:\\Users\\srnov\\Git Depositories\\Python Projects\\Python-Projects\\File_Transfer_Assn\\Folder_TBD\\'
-files = os.listdir(source)
-
-#moving all the files in "Folder_New" to "Folder_TBD"
-shutil.copytree(source,destination)
+import datetime
+import file_transfer_GUI
 
 
+#Comparing datetimes using comparison operators
+today_datetime = datetime.datetime.now()
+#Process for getting datetimes in the past. Timedelta is a change in time, then you subtract the change in time from today.
+yesterday_datetime = today_datetime - datetime.timedelta(days = 1) 
 
-if 
+
+#Parameters are necessary since this comes from out of the file
+def move_files(source, destination):
+    
+    #Getting a list of the files in the source folder
+    fileNames = os.listdir(source)
+    source += "\\"  
+    destination += "\\"
+    for i in fileNames:
+        source_path = source+i
+        #This is the timestamp, seconds from epoch
+        mod_timestamp = os.path.getmtime(source_path)
+        #This converts to a datetime
+        file_datetime = datetime.datetime.fromtimestamp(mod_timestamp)
+        
+        #Simple comparison operator that works on datetime objects
+        if file_datetime >= yesterday_datetime:
+            #Moves the file
+            shutil.move(source_path, destination) 
+
+if __name__ == "__main__":
+    pass
     
